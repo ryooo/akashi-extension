cd7791a79c07737b5bbdef265b9602f460c1a61f
ryooo <ryooo.321@gmail.com>
2022-08-02 20:09:22 +0900

    fix

--- src/index.js
+++ src/index.js
@@ -2,16 +2,25 @@ const baseDom = document.createElement('div')
 baseDom.id = "akashi-chrome-extension-history-table"
 document.querySelector(".c-column__wrapper").append(baseDom)
 
+async function getHtml(path) {
+  const res = await fetch(path, {headers: {
+    "x-requested-with": "XMLHttpRequest",
+  }});
+  const html = await res.text();
+  return html;
+}
+
 function updateTable() {
   const dt = new Date()
   const yyyy_mm_dd = (dt.getFullYear() + '-' + ('00' + (dt.getMonth()+1)).slice(-2) + '-' + ('00' + dt.getDate()).slice(-2))
-  const html = $.get(
-    "https://atnd.ak4.jp/attendance/get_punches_list?_=1659434245152&date=" + yyyy_mm_dd,
-    function(res){
-      const html = res.replace('$("#punches_list_table").html("', '').replace('");', '').replaceAll("\\\\", '\\').replaceAll("\\n", '').replaceAll("\\", '').replaceAll("showTimeZoneTooltip();", '')
-      baseDom.innerHTML = html
-    }
-  );
+  getHtml("https://atnd.ak4.jp/attendance/get_punches_list?date=" + yyyy_mm_dd)
+    .then(res => {
+      const table = res.replace('$("#punches_list_table").html("', '').replace('");', '').replaceAll("\\\\", '\\').replaceAll("\\n", '').replaceAll("\\", '').replaceAll("showTimeZoneTooltip();", '')
+      baseDom.innerHTML = table
+    })
+    .catch(err => {
+      console.log(err);
+    })
 }
 
 document.querySelectorAll(".c-circle-button").forEach(function(elem, i) {
